b5da758136f08d89444c618125cd4712ff7f0d85
LiuYanghui <liuyanghui@LiuYanghuis-Mac.local>
2017-11-01 17:20:46 +0800

    add feature
    first commit

---
+++ Example/Assets/3-LerpMoveObject/LerpMoveObjectAsset.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class LerpMoveObjectAsset : PlayableAsset
+{
+    public ExposedReference<GameObject> ObjectToMove;
+    public ExposedReference<Transform> LerpMoveTo;
+
+    public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
+    {
+        var playable = ScriptPlayable<LerpMoveObjectPlayable>.Create(graph);
+        var lerpMoveObjectPlayable = playable.GetBehaviour();
+
+        var objectToMove = ObjectToMove.Resolve(playable.GetGraph().GetResolver());
+        var lerpMoveTo = LerpMoveTo.Resolve(playable.GetGraph().GetResolver());
+
+        lerpMoveObjectPlayable.Initialize(objectToMove, lerpMoveTo);
+        return playable;
+    }
+}
