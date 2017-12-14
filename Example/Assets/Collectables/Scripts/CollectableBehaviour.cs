b672e2c37cc27d0ca54d27b16bbd837444f6f16e
LiuYanghui <liuyanghui@LiuYanghuis-Mac.local>
2017-12-14 17:04:10 +0800

    add item pickup

---
+++ Example/Assets/Collectables/Scripts/CollectableBehaviour.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class CollectableBehaviour : MonoBehaviour {
+	[Header("Playable Director References")]
+	public PlayableDirector idleTimeline;
+	public PlayableDirector collectedTimeline;
+
+	[Header("Collider")]
+	public Collider collectableCollider;
+
+	[Header("Player Settings")]
+	public string playerTag;
+	private GameObject player;
+	private ThirdPersonUserControl thirdPersonUserControl;
+	private PlayerCutsceneSpeedController playerCutsceneSpeedController;
+	public string playerAnimationTrackName;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindWithTag (playerTag);
+		thirdPersonUserControl = player.GetComponent<ThirdPersonUserControl> ();
+		playerCutsceneSpeedController = player.GetComponent<PlayerCutsceneSpeedController> ();
+		BindPlayerToAnimationTrack ();
+	}
+	
+	void BindPlayerToAnimationTrack(){
+		//Loop through timeline tracks and find the desired track to bind the player to
+		foreach (var playableAssetOutput in collectedTimeline.playableAsset.outputs) {
+			if (playableAssetOutput.streamName == playerAnimationTrackName) {
+				collectedTimeline.SetGenericBinding (playableAssetOutput.sourceObject, player);
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider theCollider){
+		if (theCollider.gameObject == player) {
+			collectableCollider.enabled = false;
+			SetPlayerRotationTowardsCollectable ();
+			idleTimeline.Stop ();
+			collectedTimeline.Play ();
+			StartCoroutine(TimelineLifeCoroutine());
+		}
+	}
+
+	void SetPlayerRotationTowardsCollectable(){
+
+		Vector3 lookDirection = transform.position - player.transform.position;
+		lookDirection.y = 0;
+		player.transform.rotation = Quaternion.LookRotation (lookDirection);
+	}
+
+	IEnumerator TimelineLifeCoroutine(){
+		playerCutsceneSpeedController.SetPlayerSpeed ();
+		thirdPersonUserControl.inputAllowed = false;
+		yield return new WaitForSeconds((float)collectedTimeline.duration);
+		thirdPersonUserControl.inputAllowed = true;
+		Destroy (gameObject);
+	}
+}
---
+++ Example/Assets/Collectables/Scripts/CollectableSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableSpawner : MonoBehaviour {
+	public GameObject[] collectablesToSpawn;
+	public KeyCode spawnKey;
+	public int xRange;
+	public int zRange;
+	public int yHeight;
+
+	void Update(){
+		if (Input.GetKeyDown (spawnKey)) {
+			SpawnObject ();
+		}
+	}
+
+	void SpawnObject(){
+		int randomCollectable = Random.Range (0, collectablesToSpawn.Length);
+		int xPosition = Random.Range (-xRange, xRange);
+		int zPosition = Random.Range (-zRange, zRange);
+		Vector3 randomPosition = new Vector3 (xPosition, yHeight, zPosition);
+		Instantiate (collectablesToSpawn [randomCollectable], transform.position + randomPosition, transform.rotation);
+	}
+}
